8d02946a4e140680abef329026602c7a68f3c321
Alden0124 <hsuchiahung24@gmail.com>
2024-12-04 00:56:02 +0800

    fix(vite.config.ts): 因為不屬後網站產生錯誤訊息跟build後配置問題有關

--- vite.config.ts
+++ vite.config.ts
@@ -17,15 +17,29 @@ export default defineConfig({
     port: 10000,
     host: "0.0.0.0",
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001/',
+      "/api": {
+        target: "http://localhost:3001/",
         changeOrigin: true,
-      }
-    }
+      },
+    },
   },
   preview: {
     port: 10000,
     host: "0.0.0.0",
   },
+  build: {
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    target: "es2015",
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
 });
-
