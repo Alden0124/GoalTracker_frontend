8a70cd852af0a4e7666107cf97ca6ea6ab6712da
Alden0124 <hsuchiahung24@gmail.com>
2024-12-10 14:04:54 +0800

    fix(googlebutton): 恢復預設google樣式設定，測試手機板登入是否可以正常

--- src/components/auth/OAuth/GoogleLoginButton.tsx
+++ src/components/auth/OAuth/GoogleLoginButton.tsx
@@ -2,7 +2,6 @@ import { FETCH_AUTH } from "@/services/api/auth";
 import { notification } from "@/utils/notification";
 import { GoogleLogin } from "@react-oauth/google";
 import { useRef } from "react";
-import { FcGoogle } from "react-icons/fc";
 // 自定義hook
 import { useSignInHandler } from "@/hooks/auth/useSignIn";
 
@@ -10,13 +9,13 @@ const GoogleLoginButton = () => {
   const buttonRef = useRef<HTMLDivElement>(null);
   const { handelSignInSucess, handleSignInError } = useSignInHandler();
 
-  const handleCustomButtonClick = () => {
-    // 程式觸發隱藏的 Google 按鈕點擊
-    const googleButton = buttonRef.current?.querySelector('div[role="button"]');
-    if (googleButton) {
-      (googleButton as HTMLElement).click();
-    }
-  };
+  // const handleCustomButtonClick = () => {
+  //   // 程式觸發隱藏的 Google 按鈕點擊
+  //   const googleButton = buttonRef.current?.querySelector('div[role="button"]');
+  //   if (googleButton) {
+  //     (googleButton as HTMLElement).click();
+  //   }
+  // };
 
   const handleSucess = async (credential: string) => {
     try {
@@ -34,7 +33,7 @@ const GoogleLoginButton = () => {
   return (
     <div className="w-full">
       {/* 隱藏的 Google 登入按鈕 */}
-      <div ref={buttonRef} className="hidden">
+      <div ref={buttonRef} >
         <GoogleLogin
           onSuccess={(credentialResponse) => {
             if (credentialResponse.credential) {
@@ -52,19 +51,20 @@ const GoogleLoginButton = () => {
           type="standard"  // 新增這行
           theme="outline"  // 新增這行
           size="large"     // 新增這行
+          width="100%"    // 新增這行
           text="signin_with"  // 新增這行
         />
       </div>
 
       {/* 自定義按鈕 */}
-      <button
+      {/* <button
         type="button"
         onClick={handleCustomButtonClick}
         className="w-full border text-center text-[16px] rounded-[5px] hover:bg-[gray]/10 dark:text-foreground-dark dark:border-gray-600 p-[6px] flex items-center justify-center gap-2"
       >
         <FcGoogle size={24} />
         使用 Google 登入
-      </button>
+      </button> */}
     </div>
   );
 };
