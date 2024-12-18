7460bb9b5cda0eebfa9f2568aee2caf04c5ce178
Alden0124 <hsuchiahung24@gmail.com>
2024-12-19 00:41:51 +0800

    fix(commentitem): 調整commtent的數量數目

--- src/services/api/Profile/ProfileGoals/constants.ts
+++ src/services/api/Profile/ProfileGoals/constants.ts
@@ -9,7 +9,7 @@ export const DEFAULT_GOALS_PARAMS: GetUserGoalsParams = {
 
 export const DEFAULT_COMMENTS_PARAMS: GetCommentsQuery = {
   page: 1,
-  limit: 10,
+  limit: 100,
   parentId: "",
   type: "progress",
 };
